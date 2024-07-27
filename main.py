03da7fffdebe021f1c57dbd68ef056b08e73b36e
jacouby <jyeenope@gmail.com>
2024-07-27 02:52:22 -0400

    why do i do this i dont even use spotify

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import requests
 import uvicorn
 import json
-from fastapi import FastAPI, UploadFile, Header
+import os, io
+from PIL import Image
+from fastapi import FastAPI, UploadFile, Header, File
 from pydantic import BaseModel
 from typing import Annotated
+
+image_folder = './images'
 
 app = FastAPI()
 
@@ -16,15 +20,37 @@ class track(BaseModel):
 
 @app.get("/")
 async def grt_root():
-     return('This server is only for use with the shortcut!')
+     current = open('./data.json')
+     template = open('./dt.json')
+
+     if (current.read() != template.read()):
+          current.truncate(0)
+          current.write(template.read())
+     current.close()
+     template.close()
+
+
+     return('Server Ready')
 
 @app.post("/")
-async def main(image: UploadFile, artist: Annotated[str | None, Header()] = None, title: Annotated[str | None, Header()] = None, explicit: Annotated[str | None, Header()] = None, playlist: Annotated[str | None, Header()] = None, index: Annotated[str | None, Header()] = None,):
+async def main(image: Annotated[bytes, File()], artist: Annotated[str | None, Header()] = None, title: Annotated[str | None, Header()] = None, explicit: Annotated[str | None, Header()] = None, playlist: Annotated[str | None, Header()] = None, index: Annotated[str | None, Header()] = None, total: Annotated[str | None, Header()] = None):
      status = 0
+     
+     import os
+     # Check if the image_folder exists
+     if not os.path.exists(image_folder):
+     # Create the image_folder
+          os.makedirs(image_folder)
+          print(f"Dir '{image_folder}' created.")
 
-     print(f"Playlist: {playlist}, {image.filename}, ({index}) songs: {artist} - {title}")
-     image.read()
-     #print(current_track)
+
+     print(f"Playlist: {playlist} ({index}/{total}) SONG: {artist} - {title}")
+     #image.read()
+     #open(f'{image_folder}/{index}a.jpeg', 'w').write(await image.read())
+     Image.open(io.BytesIO(await image)).save(f'{image_folder}/{index}a.jpeg', 'JPEG')
+     #print(current_track)3``
+     
+     db = json.loads(await open('./data.json').read())
      
      
      return {"status": status}
